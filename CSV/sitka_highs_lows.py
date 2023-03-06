9a2684677130d9d4148fd22fe0e1d510eee818c1
tagiroid <tagiroid@gmail.com>
2023-03-06 21:25:54 +0500

    highs and lows

---
+++ CSV/sitka_highs_lows.py
@@ -0,0 +1,31 @@
+import csv
+from matplotlib import pyplot as plt
+from datetime import datetime
+
+filename = 'data/sitka_weather_2021_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    dates, highs, lows = [], [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        high = int(row[4])
+        low = int(row[5])
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
+
+plt.style.use('classic')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+plt.title('Daily high and low temperatures - 2021', fontsize=24)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel('Temperature (F)', fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
